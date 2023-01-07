9c3ec76c5ed5ef0bf3630e3468fe6ed1359f8c70
GautamiDhokte1999 <gautamid@winjit.com>
2023-01-08 02:06:27 +0530

    navigation and react native paper

---
+++ template/src/App.js
@@ -0,0 +1,7 @@
+import ApplicationNavigator from './Navigators/Application';
+import React from 'react';
+import 'react-native-gesture-handler';
+
+const App = () => <ApplicationNavigator />;
+
+export default App;
